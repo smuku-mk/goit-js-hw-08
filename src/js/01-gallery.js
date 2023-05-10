dc9ea4bdbec8f0e7ae65ddea203323a1bf4cdd0e
Mateusz Kubiak <smuku.mk@gmail.com>
2023-05-10 22:18:43 +0200

    task_01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const list = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(
+    image =>
+      `<li style="font-size:0">
+        <a class="gallery__item" href="${image.original}">
+          <img
+            src="${image.preview}" 
+            alt="${image.description}" 
+            class="gallery__image"
+          />
+        </a>
+      </li>`
+  )
+  .join('');
+list.insertAdjacentHTML('afterbegin', markup);
 
-console.log(galleryItems);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionType: 'alt',
+});
