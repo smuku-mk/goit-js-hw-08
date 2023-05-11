b14c8cfc7ca7dad2b258da0e6175fc3fea6d05e0
Mateusz Kubiak <smuku.mk@gmail.com>
2023-05-11 12:04:50 +0200

    task_02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const getDurationTime = data => {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', throttle(getDurationTime, 1000));
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
